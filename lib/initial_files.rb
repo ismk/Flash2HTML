c54e5348772f76d1f8362d1d345fbdf19bb930fa
Michael Ginalick <michaelginalick@Michael-G-E00340.local>
2016-02-09 18:00:08 -0600

    updated to make wallboard assets

--- lib/initial_files.rb
+++ lib/initial_files.rb
@@ -31,10 +31,36 @@ module F2h
       exec('curl -o js/TweenMax.min.js http://cdnjs.cloudflare.com/ajax/libs/gsap/1.17.0/TweenMax.min.js;curl -o js/jquery.min.js https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0-alpha1/jquery.min.js')
     end
 
+    def make_wallboard_files
+      make_wallboard_dirs
+      @types.drop(1).each do |t|
+        @ext.each do |k,value|
+          if value
+            File.open("#{k}/"+@get_current_foldername+"_#{t}.#{k}", 'w+') do |file|
+              k.eql?("js") ? t.eql?("box") ? to_write = @bp.box_js_boilerplate : to_write = @bp.wallboard_banner_js_boilerplate : t.eql?("box") ? to_write = @bp.box_css_boilerplate : to_write = @bp.wallboard_banner_css_boilerplate
+                # file.write(send("#{@bp}.#{t}_#{k}_boilerplate"))
+              file.write(to_write)
+            end
+          else
+            File.open(@get_current_foldername+"_#{t}.#{k}", 'w+') do |file|
+              to_write = @bp.wallboard_html_biolerplate(@get_current_foldername, t)
+              file.write(to_write)
+            end
+          end
+        end
+      end
+      exec('curl -o js/TweenMax.min.js http://cdnjs.cloudflare.com/ajax/libs/gsap/1.17.0/TweenMax.min.js;curl -o js/jquery.min.js https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0-alpha1/jquery.min.js')
+    end
+
     def make_dirs
       Dir.mkdir("js") unless Dir.exist?("js")
       Dir.mkdir("css") unless Dir.exist?("css")
       Dir.mkdir("box_images") unless Dir.exist?("box_images")
+      Dir.mkdir("banner_images") unless Dir.exist?("banner_images")
+    end
+    def make_wallboard_dirs
+      Dir.mkdir("js") unless Dir.exist?("js")
+      Dir.mkdir("css") unless Dir.exist?("css")
       Dir.mkdir("banner_images") unless Dir.exist?("banner_images")
     end
   end
